f999fe482a356b1949fabfd05091c976b6e1939f
Dmitri Gorun <iii448@mail.ru>
2021-10-02 02:18:24 +0500

    lesson5-react-hooks

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import styled from 'styled-components';
+import bannerImg from '../img/banner.png';
+
+export const Banner = styled.div`
+  margin-top: 80px;
+  width: 100%;
+  height: 370px;
+  background-image: url(${bannerImg});
+  background-position: center;
+  background-size: cover;
+`;
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import dbMenu from './DBMenu';
 import { ListItem } from './ListItem';
+import { Banner } from './Banner';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,33 +15,24 @@ const SectionMenu = styled.section`
   padding: 30px;
 `;
 
-const BannerMenu = styled.div`
-  margin-top: 80px;
-  width: 100%;
-  height: 370px;
-  background-image: url(banner.png);
-  background-position: center;
-  background-size: cover;
-`;
-
-export const Menu = () => (
+export const Menu = ({ setOpenItem }) => (
   <MenuStyled>
-    <BannerMenu />
+    <Banner />
     <SectionMenu>
       <h2>Бургеры</h2>
-      <ListItem itemList={dbMenu.burger} />
+      <ListItem
+        itemList={dbMenu.burger}
+        setOpenItem={setOpenItem}
+      />
     </SectionMenu>
 
     <SectionMenu>
       <h2>Закуски / Напитки</h2>
-      <ListItem itemList={dbMenu.other} />
+      <ListItem
+        itemList={dbMenu.other}
+        setOpenItem={setOpenItem}
+      />
     </SectionMenu>
 
   </MenuStyled>
 );
-
-
-
-
-
-
